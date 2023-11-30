2cc9303f5fc5270feb7b0de4c799520d3d149c07
Ozod <o.bukhorizoda@mail.ru>
2023-11-30 22:56:44 +0500

    made an async function to get data from advice api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const API = "https://api.adviceslip.com/advice";
+  const [advice, setAdvice] = useState("");
+
+  const getAdvice = async () => {
+    try {
+      const response = await fetch(API);
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  return (
+    <div>
+      <button onClick={() => getAdvice()}>Click me</button>
+      <p>{advice}</p>
+    </div>
+  );
+}
 
 export default App;
